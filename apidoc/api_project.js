5ecb5273d00a3351c79291a0a7bcdfad4903ceb5
François Debert <francois.debert@epitech.eu>
2021-11-21 18:25:45 +0100

    [TEST] deploy to prod

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-11-21T17:04:15.754Z",
+    "time": "2021-11-21T17:24:48.833Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
